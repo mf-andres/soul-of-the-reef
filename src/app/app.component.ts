dc3c96f0367785fbc7b2bcb8548b37ef3e104ca0
mf-andres <amunoz@gradiant.org>
2024-08-25 19:34:27 +0200

    chore: layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,37 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'soul-of-the-reef';
+  name = '';
+
+  strength = 1;
+  agility = 1;
+  will = 1;
+  eye = 1;
+
+  tool1 = '';
+  tool2 = '';
+  tool3 = '';
+  tool4 = '';
+  tool5 = '';
+  tool6 = '';
+
+  merch1 = '';
+  merch2 = '';
+  merch3 = '';
+  merch4 = '';
+  merch5 = '';
+  merch6 = '';
+
+  body = '';
+  detail = '';
+  hair = '';
+  age = '';
+
+  virtue = '';
+  vice = '';
+  tragedy = '';
+  background = '';
 }
